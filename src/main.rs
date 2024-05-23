8077ed75199c56f739ed06bb35320e3792303b39
Mikhail Matyushev <mikhail.matyushev@gmail.com>
2024-05-23 23:04:55 +0300

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod parser;
+
+use parser::Parser;
 use std::{
     env,
     fs::File,
@@ -5,254 +8,43 @@ use std::{
     process::Command,
 };
 
-trait RemoveWs {
-    fn remove_whitespaces(&self) -> String;
-    fn remove_tab(&self) -> String;
-}
-
-impl RemoveWs for String {
-    fn remove_whitespaces(&self) -> String {
-        self.chars().filter(|&c| c != '\n').collect()
-    }
-
-    fn remove_tab(&self) -> String {
-        let mut res = String::new();
-        
-        if !self.is_empty() {
-            let mut prev: char = self[..1].chars().collect::<Vec<char>>()[0];
-
-            self.chars().for_each(|c| {
-                if c == prev {
-                    return;
-                }
-
-                prev = c;
-                res.push(c);
-            });
-        }
-
-        res
-    }
-}
-
-fn parse_call(src: &mut String, method: String) {
-    let mut bounds: Vec<(usize, usize, String)> = vec![];
-
-    for (i, expr) in src.clone().match_indices(&format!("->{}", method)) {
-        let mut offset: usize = expr.len() + 1;
-
-        let var: String = src[..i]
-            .chars()
-            .rev()
-            .take_while(|&c| c != ' ' && c != '\n' && c != ',')
-            .collect();
-
-        let mut args: String = src[i..]
-            .chars()
-            .skip(offset)
-            .take_while(|&c| c != ')')
-            .collect();
-
-        offset += args.len() + 1;
-
-        if !args.is_empty() {
-            args.insert_str(0, ", ");
-        }
-
-        let expr = format!(
-            "{}({}{})",
-            expr,
-            var.chars().rev().collect::<String>(),
-            args
-        );
-
-        bounds.push((i, i + offset, expr));
-
-        println!("call:\n\tmethod: {}\n\targs: {}\n", method, args);
-    }
-
-    for (start, end, expr) in bounds.iter().rev() {
-        src.replace_range(start..end, expr)
-    }
-}
-
-fn parse_method(src: &mut String, class: String) {
-    let mut bounds: Vec<(usize, usize, String)> = vec![];
-
-    for (i, expr) in src.clone().match_indices(&format!("{}::", class)) {
-        let mut offset: usize = expr.len();
-
-        let ty: String = src[..i].chars().rev().take_while(|&c| c != '\n').collect();
-
-        let name: String = src[i..]
-            .chars()
-            .skip(offset)
-            .take_while(|&c| c != '(')
-            .collect();
-
-        offset += name.len() + 1;
-
-        let params: String = src[i..]
-            .chars()
-            .skip(offset)
-            .take_while(|&c| c != ')')
-            .collect();
-
-        offset += params.len() + 3;
-
-        let body: String = src[i..]
-            .chars()
-            .skip(offset)
-            .take_while(|&c| c != '}')
-            .collect();
-
-        offset += body.len() + 1;
-
-        let expr = format!(
-            include_str!("tmpl/method.tmpl"),
-            class.trim(),
-            ty.chars().rev().collect::<String>().trim(),
-            name.trim(),
-            params.replace(",", ";"),
-            body.trim()
-        );
-
-        bounds.push((i - ty.len(), i + offset, expr));
-        parse_call(src, name.clone());
-
-        println!(
-            "method:\n\tty: {}\n\tname: {}\n\tparams: {}\n\tbody: {}\n",
-            ty.chars().rev().collect::<String>(),
-            name,
-            params.remove_whitespaces().remove_tab(),
-            body.remove_whitespaces().remove_tab()
-        );
-    }
-
-    for (start, end, expr) in bounds.iter().rev() {
-        src.replace_range(start..end, expr);
-    }
-}
-
-fn parse_constructor(src: &mut String) {
-    let mut bounds: Vec<(usize, usize, String)> = vec![];
-
-    for (i, op) in src.clone().match_indices("new") {
-        let mut offset: usize = op.len() + 1;
-
-        let class: String = src[i..]
-            .chars()
-            .skip(offset)
-            .take_while(|&c| c != '(')
-            .collect();
-
-        offset += class.len() + 1;
-
-        let body: String = src[i..]
-            .chars()
-            .skip(offset)
-            .take_while(|&c| c != ')')
-            .collect();
-
-        offset += body.len() + 2;
-
-        let expr = format!(include_str!("tmpl/new.tmpl"), class, body);
-
-        bounds.push((i, i + offset, expr));
-
-        println!("intialization:\n\tclass: {}\n\targs: {}", class, body);
-    }
-
-    for (start, end, expr) in bounds.iter().rev() {
-        src.replace_range(start..end, expr);
-    }
-}
-
-fn parse_classes(src: &mut String) {
-    let mut bounds: Vec<(usize, usize, String)> = vec![];
-
-    for (i, op) in src.clone().match_indices("class") {
-        let mut offset: usize = op.len() + 1;
-
-        let name: String = src[i..]
-            .chars()
-            .skip(offset)
-            .take_while(|&c| c != ' ' && c != '{')
-            .collect();
-
-        offset += name.len() + 1;
-
-        let parent: String = src[i..]
-            .chars()
-            .skip(offset)
-            .take_while(|&c| c != '{')
-            .collect();
-
-        offset += parent.len() + 1;
-
-        let mut body: String = src[i..]
-            .chars()
-            .skip(offset)
-            .take_while(|&c| c != '}')
-            .collect();
-
-        offset += body.len() + 1;
-
-        if parent.trim() != "" {
-            body.push_str(&format!("\n  {} super;\n", parent.trim_matches('+')));
-        }
-
-        let expr = format!(include_str!("tmpl/class.tmpl"), name, &body[1..]);
-
-        bounds.push((i, i + offset, expr));
-
-        parse_method(src, name.clone().trim().to_string());
-
-        println!(
-            "class declaration\n\tclass: {}\n\tfields: {}\n",
-            name.trim(),
-            body.remove_tab().remove_whitespaces()
-        );
-    }
-
-    for (start, end, expr) in bounds.iter().rev() {
-        src.replace_range(start..end, expr);
-    }
-
-    src.insert_str(0, "#include \"src/c/include/class.h\"\n");
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        eprintln!("USAGE {} FILE", args[0]);
+    if args.len() < 4 {
+        println!(
+            "PreC Preprocessor {}\n\nUSAGE: {} [OPTIONS] FILE\n\nOPTIONS:\n-h, --help      : print help\n-o, --output    : output file",
+            env!("CARGO_PKG_VERSION"),
+            args[0]
+        );
         return;
     }
 
-    if !args[1].ends_with(".preC") {
+    if !args[3].ends_with(".preC") {
         eprintln!("FILE must be PreC source file");
         return;
     }
 
     let mut buffer = String::new();
 
-    match File::open(args[1].clone()).and_then(|mut file| file.read_to_string(&mut buffer)) {
+    match File::open(&args[3]).and_then(|mut file| file.read_to_string(&mut buffer)) {
         Ok(_) => {
-            parse_classes(&mut buffer);
-            parse_constructor(&mut buffer);
+            let mut parser = Parser::new(buffer);
 
-            // buffer = buffer.remove_end_line();
+            parser.parse();
 
-            match File::create("pre.c").and_then(|mut file| file.write(buffer.as_bytes())) {
+            match File::create("pre.c").and_then(|mut file| file.write(parser.src.as_bytes())) {
                 Ok(_) => {
-                    Command::new("clang")
-                        .args(["-o", "program.exe", "pre.c"])
+                    let compile = Command::new("clang")
+                        .args(["-o", args[2].as_str(), "pre.c"])
                         .output()
-                        .expect("msg");
-                    buffer.clear();
+                        .expect("Failed to run clang");
+
+                    if compile.status.success() {
+                        let _ = std::fs::remove_file("pre.c");
+                    }
                 }
-                Err(_) => eprintln!("Failed to run compiled program"),
+                Err(_) => eprintln!("Failed to create output file"),
             }
         }
         Err(_) => eprintln!("Unable to read source file"),
---
+++ src/parser.rs
@@ -0,0 +1,220 @@
+#[derive(Debug)]
+struct Class {
+    name: String,
+    methods: Vec<String>,
+}
+
+pub struct Parser {
+    pub src: String,
+    classes: Vec<Class>,
+}
+
+impl Parser {
+    pub fn new(src: String) -> Self {
+        Self {
+            src,
+            classes: vec![],
+        }
+    }
+
+    fn collect_until<F>(&self, start: usize, n: usize, condition: F) -> String
+    where
+        F: Fn(char) -> bool,
+    {
+        self.src[start..]
+            .chars()
+            .skip(n)
+            .take_while(|&c| !condition(c))
+            .collect()
+    }
+
+    fn insert(&mut self, bounds: Vec<(usize, usize, String)>) {
+        for (start, end, expr) in bounds.iter().rev() {
+            self.src.replace_range(*start..*end, expr);
+        }
+    }
+
+    pub fn parse(&mut self) {
+        self.parse_classes();
+        self.parse_constructor();
+    }
+
+    fn parse_call(&mut self, method: String) {
+        let mut bounds: Vec<(usize, usize, String)> = vec![];
+
+        for (i, expr) in self.src.clone().match_indices(&format!("->{}", method)) {
+            let mut offset: usize = expr.len() + 1;
+
+            let var: String = self.src[..i]
+                .chars()
+                .rev()
+                .take_while(|&c| c != ' ' && c != '\n' && c != ',')
+                .collect();
+
+            let mut args: String = self.collect_until(i, offset, |c| c == ')');
+
+            offset += args.len() + 1;
+
+            if !args.is_empty() {
+                args.insert_str(0, ", ");
+            }
+
+            let expr = format!(
+                "{}({}{})",
+                expr,
+                var.chars().rev().collect::<String>(),
+                args
+            );
+
+            bounds.push((i, i + offset, expr));
+
+            dbg!(method.clone(), args);
+        }
+
+        self.insert(bounds);
+    }
+
+    fn parse_method(&mut self, class: String) {
+        let mut bounds: Vec<(usize, usize, String)> = vec![];
+
+        for (i, expr) in self.src.clone().match_indices(&format!("{}::", class)) {
+            let mut offset: usize = expr.len();
+
+            let ty: String = self.src[..i]
+                .chars()
+                .rev()
+                .take_while(|&c| c != '\n')
+                .collect();
+
+            let name: String = self.collect_until(i, offset, |c| c == '(');
+
+            offset += name.len() + 1;
+
+            let params: String = self.collect_until(i, offset, |c| c == ')');
+
+            offset += params.len() + 3;
+
+            let mut body: String = self.collect_until(i, offset, |c| c == '}');
+
+            offset += body.len() + 1;
+
+            body = body.replace("super", "self->super");
+
+            let expr = format!(
+                include_str!("tmpl/method.tmpl"),
+                class.trim(),
+                ty.chars().rev().collect::<String>().trim(),
+                name.trim(),
+                params.replace(",", ";"),
+                body
+            );
+
+            bounds.push((i - ty.len(), i + offset, expr));
+
+            if let Some(class) = self.classes.iter_mut().find(|c| c.name == class) {
+                if !class.methods.contains(&name) {
+                    class.methods.push(name.clone());
+                }
+            } else {
+                self.classes.push(Class {
+                    name: class.clone(),
+                    methods: vec![name.clone()],
+                });
+            }
+
+
+            self.parse_call(name.clone());
+
+            dbg!(ty.chars().rev().collect::<String>(), name, params, body);
+        }
+
+        self.insert(bounds);
+    }
+
+    fn parse_constructor(&mut self) {
+        let mut bounds: Vec<(usize, usize, String)> = vec![];
+
+        for (i, op) in self.src.clone().match_indices("new") {
+            let mut offset: usize = op.len() + 1;
+
+            let class: String = self.collect_until(i, offset, |c| c == '(');
+
+            offset += class.len() + 1;
+
+            let mut body: String = self.collect_until(i, offset, |c| c == ')');
+
+            offset += body.len() + 2;
+
+            if let Some(class) = self.classes.iter().find(|c| c.name == class) {
+                let body_start: String = body.chars().take_while(|&c| c != '{').collect();
+                let body_end: String = body.chars().rev().take_while(|&c| c != ',').collect();
+
+                let mut methods = String::new();
+
+                class.methods.iter().enumerate().for_each(|(i, m)| {
+                    methods.push_str(&format!("{}_{},", class.name, m));
+
+                    if i != class.methods.len() - 1 {
+                        methods.push_str(" ");
+                    }
+                });
+
+                body = format!(
+                    "{}{}{}",
+                    body_start,
+                    methods,
+                    body_end.chars().rev().collect::<String>()
+                );
+            }
+
+            let expr = format!(include_str!("tmpl/new.tmpl"), class, body);
+
+            bounds.push((i, i + offset, expr));
+
+            dbg!(class, body);
+        }
+
+        self.insert(bounds);
+    }
+
+    fn parse_classes(&mut self) {
+        let mut bounds: Vec<(usize, usize, String)> = vec![];
+
+        for (i, op) in self.src.clone().match_indices("class") {
+            let mut offset: usize = op.len() + 1;
+
+            let name: String = self.collect_until(i, offset, |c| c == ' ' || c == '{');
+
+            offset += name.len() + 1;
+
+            let parent: String = self.collect_until(i, offset, |c| c == '{');
+
+            offset += parent.len() + 1;
+
+            let mut body: String = self.collect_until(i, offset, |c| c == '}');
+
+            offset += body.len() + 1;
+
+            if !parent.trim().is_empty() {
+                body.push_str(&format!("\n\t{} super;\n", parent.trim_matches('+').trim()));
+            }
+
+            let expr = format!(include_str!("tmpl/class.tmpl"), name, &body[1..]);
+
+            bounds.push((i, i + offset, expr));
+
+            self.classes.push(Class {
+                name: name.clone(),
+                methods: vec![],
+            });
+            self.parse_method(name.clone().trim().to_string());
+
+            dbg!(name, body);
+        }
+
+        self.insert(bounds);
+
+        self.src
+            .insert_str(0, "#include \"src/c/include/class.h\"\n");
+    }
+}
